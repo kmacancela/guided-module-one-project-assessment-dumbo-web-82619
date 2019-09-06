1fde3cfe59252667adca79806f9e00488bac40d9
Karina Macancela <karymacancela@gmail.com>
2019-09-06 17:26:01 -0400

    Adding method to User model and prompt to run file

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
     has_many :posts
     has_many :books, through: :posts
+
+    def user_posts
+        self.posts.select do |post|
+            post.user_id == self.id
+        end
+    end
 end
